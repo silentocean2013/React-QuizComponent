c11cbd6ae5b3685099ebd5813685b7c8d98729c0
Sagar Kulung <M1049161@mindtree.com>
2020-08-04 15:12:06 +0530

    submit 4

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import  QuizQuestion from './QuizQuestion.js';
-
+import QuizEnd from './QuizEnd.js';
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
@@ -11,13 +11,26 @@ class Quiz extends Component {
     }
     
     render() {
+        const isQuizEnd = (this.state.quiz_position-1) === quizData.quiz_questions.length;
+        if (isQuizEnd){
         return (
             <div>
+                <QuizEnd />
                 <QuizQuestion 
                     quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
                 />
             </div>
         )
+    } else {
+        return (
+            <div>
+               
+                <QuizQuestion 
+                    quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
+                />
+            </div>
+        )
+    }
     }
 
 }
